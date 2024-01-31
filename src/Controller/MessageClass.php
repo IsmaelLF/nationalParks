8415d54b02cd00c941e725fd05d224fe0cad920e
A22IsmaelLF <A22IsmaelLF@iessanclemente.net>
2024-01-31 13:12:31 +0100

    a

--- src/Controller/MessageClass.php
+++ src/Controller/MessageClass.php
@@ -3,20 +3,25 @@
 namespace App\Controller;
 
 use App\Service\CreateMessage;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MessageClass{
-
-    public function __construct(private CreateMessage $createMessage){
-
+class MessageClass extends AbstractController
+{
+    public function __construct(private CreateMessage $createMessage)
+    {
     }
-    #[Route('/message/{data}' , name:'app_message')]
-    public function sendMessage($data = null){
 
-        if($data){
-            $this->createMessage->messageToLogger($data);
+    #[Route('/message/{data}', name: 'app_message')]
+    public function sendMessage($data = null): Response
+    {
+        $result = '';
+
+        if ($data) {
+            $result = $this->createMessage->messageToLogger($data);
         }
 
+        return new Response($result);
     }
-
 }
